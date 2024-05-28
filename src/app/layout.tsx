114309255c28df8cb5bba40969e50b8c7a9e24d4
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-05-28 16:52:40 +0300

    Added win context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './globals.css';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
+import { WixClientContextProvider } from '@/context/wixContext';
+
+import './globals.css';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,9 +21,11 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={inter.className}>
-                <Navbar />
-                {children}
-                <Footer/>
+                <WixClientContextProvider>
+                    <Navbar />
+                    {children}
+                    <Footer />
+                </WixClientContextProvider>
             </body>
         </html>
     );
