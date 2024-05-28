a73624f5a60c66c11bf2fde7e80b410a3837d228
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-05-28 15:42:53 +0300

    Added Add product component

---
+++ src/components/Add.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import { useState } from 'react';
+
+const Add = () => {
+    const [quantity, setQuantity] = useState(0);
+    const stock = 4;
+
+    return (
+        <div className="flex flex-col gap-4">
+            <h4 className="font-medium">Choose a Quantity</h4>
+            <div className="flex justify-between">
+                <div className="flex items-center gap-4">
+                    <div className="bg-gray-100 py-2 px-4 rounded-3xl flex items-center justify-between w-32">
+                        <button
+                            className="cursor-pointer text-xl disabled:cursor-not-allowed disabled:opacity-20"
+                        >
+                            -
+                        </button>
+                        {quantity}
+                        <button
+                            className="cursor-pointer text-xl disabled:cursor-not-allowed disabled:opacity-20"
+                        >
+                            +
+                        </button>
+                    </div>
+                    {stock < 1
+                        ? (
+                            <div className="text-xs">Product is out of stock</div>
+                        ) : (
+                            <div className="text-xs">
+                                Only <span className="text-orange-500">{stock} items</span>{" "}
+                                left!
+                                <br /> {"Don't"} miss it
+                            </div>
+                        )
+                    }
+                </div>
+                <button className="w-36 text-sm rounded-3xl ring-1 ring-era text-era py-2 px-4 hover:bg-era hover:text-white disabled:cursor-not-allowed disabled:bg-pink-200 disabled:ring-0 disabled:text-white disabled:ring-none">
+                    Add to Cart
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Add;
