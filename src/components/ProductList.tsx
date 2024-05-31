dbf5fff1c3445724bf532ab783b0b98baa9d5ef9
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-05-31 14:12:04 +0300

    Added filter collection functionality

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -8,11 +8,29 @@ const PRODUCT_PER_PAGE = 12;
 
 const ProductList = async ({ categoryId, limit, searchParams }: { categoryId: string; limit?: number; searchParams?: any; }) => {
     const wixClient = await wixClientServer();
-    const res = await wixClient.products
+
+    const productQuery = wixClient.products
         .queryProducts()
+        .startsWith('name', searchParams?.name || '')
         .eq('collectionIds', categoryId)
-        .limit(limit || PRODUCT_PER_PAGE)
-        .find();
+        .hasSome('productType', searchParams?.type ? [searchParams.type] : ['physical', 'digital'])
+        .gt('priceData.price', searchParams?.min || 0)
+        .lt('priceData.price', searchParams?.max || 999999)
+        .limit(limit || PRODUCT_PER_PAGE);
+
+    if (searchParams?.sort) {
+        const [sortType, sortBy] = searchParams.sort.split(' ');
+
+        if (sortType === 'asc') {
+            productQuery.ascending(sortBy);
+        }
+
+        if (sortType === 'desc') {
+            productQuery.descending(sortBy);
+        }
+    }
+
+    const res = await productQuery.find();
 
     return (
         <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
