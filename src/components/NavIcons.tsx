58e2e8fe7936bfb274a7de21d8830df80c6950d2
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-06-02 22:51:43 +0300

    Added authentication with google and facebook profiles

--- src/components/NavIcons.tsx
+++ src/components/NavIcons.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import CartModal from './CartModal';
+import { useWixClient } from '@/hooks/useWixClient';
 
 const NavIcons = () => {
     const [isProfileOpen, setIsProfileOpen] = useState(false);
@@ -22,6 +23,20 @@ const NavIcons = () => {
         }
     };
 
+    const wixClient = useWixClient();
+
+    const login = async () => {
+        const loginRequestData = wixClient.auth.generateOAuthData(
+            'http://localhost:3000'
+        );
+
+        console.log(loginRequestData);
+
+        localStorage.setItem('oAuthRedirectData', JSON.stringify(loginRequestData));
+        const { authUrl } = await wixClient.auth.getAuthUrl(loginRequestData);
+        window.location.href = authUrl;
+    };
+
     return (
         <div className="flex items-center gap-4 xl:gap-6 relative">
             <Image
@@ -30,7 +45,8 @@ const NavIcons = () => {
                 width={22}
                 height={22}
                 className="cursor-pointer"
-                onClick={handleProfile}
+                // onClick={handleProfile}
+                onClick={login}
             />
             {isProfileOpen && (
                 <div className="absolute p-4 rounded-md top-12 left-0 bg-white text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] z-20">
