0e2432a22bbc81888a54074f418840a5c21c9f96
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-06-03 21:05:12 +0300

    Added Logout functionality

--- src/components/NavIcons.tsx
+++ src/components/NavIcons.tsx
@@ -6,14 +6,17 @@ import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import CartModal from './CartModal';
 import { useWixClient } from '@/hooks/useWixClient';
+import Cookies from 'js-cookie';
 
 const NavIcons = () => {
     const [isProfileOpen, setIsProfileOpen] = useState(false);
     const [isCartOpen, setIsCartOpen] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
 
     const router = useRouter();
 
-    const isLoggedIn = false;
+    const wixClient = useWixClient();
+    const isLoggedIn = wixClient.auth.loggedIn();
 
     const handleProfile = () => {
         if (!isLoggedIn) {
@@ -38,6 +41,15 @@ const NavIcons = () => {
     //     window.location.href = authUrl;
     // };
 
+    const handleLogout = async () => {
+        setIsLoading(true);
+        Cookies.remove('refreshToken');
+        const { logoutUrl } = await wixClient.auth.logout(window.location.href);
+        setIsLoading(false);
+        setIsProfileOpen(false);
+        router.push(logoutUrl);
+    };
+
     return (
         <div className="flex items-center gap-4 xl:gap-6 relative">
             <Image
@@ -47,13 +59,13 @@ const NavIcons = () => {
                 height={22}
                 className="cursor-pointer"
                 onClick={handleProfile}
-                // onClick={login}
+            // onClick={login}
             />
             {isProfileOpen && (
                 <div className="absolute p-4 rounded-md top-12 left-0 bg-white text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] z-20">
                     <Link href="/profile">Profile</Link>
-                    <div className="mt-2 cursor-pointer">
-                        Logout
+                    <div onClick={handleLogout} className="mt-2 cursor-pointer">
+                        {isLoading ? 'Logging out' : 'Logout'}
                     </div>
                 </div>
             )}
