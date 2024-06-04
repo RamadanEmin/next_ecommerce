ecfc12a0d82a234fb1aaeec5d7784f8d798189d3
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-06-04 15:17:28 +0300

    Updated basket with product counter

--- src/components/NavIcons.tsx
+++ src/components/NavIcons.tsx
@@ -1,12 +1,13 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import CartModal from './CartModal';
 import { useWixClient } from '@/hooks/useWixClient';
 import Cookies from 'js-cookie';
+import { useCartStore } from '@/hooks/useCartStore';
 
 const NavIcons = () => {
     const [isProfileOpen, setIsProfileOpen] = useState(false);
@@ -50,6 +51,12 @@ const NavIcons = () => {
         router.push(logoutUrl);
     };
 
+    const { counter, getCart } = useCartStore();
+
+    useEffect(() => {
+        getCart(wixClient);
+    }, [wixClient, getCart]);
+
     return (
         <div className="flex items-center gap-4 xl:gap-6 relative">
             <Image
@@ -82,7 +89,7 @@ const NavIcons = () => {
             >
                 <Image src="/cart.png" alt="" width={22} height={22} />
                 <div className="absolute -top-4 -right-4 w-6 h-6 bg-era rounded-full text-white text-sm flex items-center justify-center">
-                    2
+                    {counter}
                 </div>
             </div>
             {isCartOpen && <CartModal />}
