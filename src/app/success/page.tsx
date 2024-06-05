5bcb41c052a7103aaea6fc8622f01b60b2356c28
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-06-05 15:34:41 +0300

    Added routing to order page

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,8 +1,28 @@
 'use client';
 
+import { useEffect } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
 import Confetti from 'react-confetti';
 
 const SuccessPage = () => {
+    const searchParams = useSearchParams();
+    const router = useRouter();
+
+    const orderId = searchParams.get('orderId');
+
+    useEffect(() => {
+        if (!orderId) {
+            return;
+        }
+
+        const timer = setTimeout(() => {
+            router.push('/orders/' + orderId);
+        }, 5000);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [orderId, router]);
 
     return (
         <div className="flex flex-col gap-6 items-center justify-center h-[calc(100vh-180px)]">
