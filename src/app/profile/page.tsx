c0e673c1a4af95c3be31d36830470660c12d7755
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-06-06 14:04:15 +0300

    Added User profile form

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -25,7 +25,48 @@ const ProfilePage = async () => {
     return (
         <div className="flex flex-col md:flex-row gap-24 md:h-[calc(100vh-180px)] items-center px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
             <div className="w-full md:w-1/2">
-                User
+                <h1 className="text-2xl">Profile</h1>
+                <form action="" className="mt-12 flex flex-col gap-4">
+                    <label className="text-sm text-gray-700">Username</label>
+                    <input
+                        type="text"
+                        name="username"
+                        placeholder={user.member?.profile?.nickname || 'john'}
+                        className="ring-1 ring-gray-300 rounded-md p-2 max-w-96"
+                    />
+                    <label className="text-sm text-gray-700">First Name</label>
+                    <input
+                        type="text"
+                        name="firstName"
+                        placeholder={user.member?.contact?.firstName || 'John'}
+                        className="ring-1 ring-gray-300 rounded-md p-2 max-w-96"
+                    />
+                    <label className="text-sm text-gray-700">Surname</label>
+                    <input
+                        type="text"
+                        name="lastName"
+                        placeholder={user.member?.contact?.lastName || 'Doe'}
+                        className="ring-1 ring-gray-300 rounded-md p-2 max-w-96"
+                    />
+                    <label className="text-sm text-gray-700">Phone</label>
+                    <input
+                        type="text"
+                        name="phone"
+                        placeholder={
+                            (user.member?.contact?.phones &&
+                                user.member?.contact?.phones[0]) ||
+                            '+359111222333'
+                        }
+                        className="ring-1 ring-gray-300 rounded-md p-2 max-w-96"
+                    />
+                    <label className="text-sm text-gray-700">E-mail</label>
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder={user.member?.loginEmail || 'john@gmail.com'}
+                        className="ring-1 ring-gray-300 rounded-md p-2 max-w-96"
+                    />
+                </form>
             </div>
             <div className="w-full md:w-1/2">
                 <h1 className="text-2xl">Orders</h1>
